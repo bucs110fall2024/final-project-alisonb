8c1b811f6b405d0e4dee61e0b3b0405fb4637afb
Alison Batz <abatz@binghamton.edu>
2024-11-20 22:25:37 -0500

    Updated! Still needs work

--- template_final_project-master/src/yarn.py
+++ template_final_project-master/src/yarn.py
@@ -13,6 +13,7 @@ class Yarn(pygame.sprite.Sprite):
     def motion(self):
         pass
     #Need to integrate button with the hook; how do I make it look as if its moving?
+    #I assume I need to use blitzing 
         
     def save_state(self):
         yarn_state = self.__dict__
