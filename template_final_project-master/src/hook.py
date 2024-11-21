c4bfaf3e7f9b227ac06f779c2433459b26c6cb3b
Alison Batz <abatz@binghamton.edu>
2024-11-20 22:22:30 -0500

    Updated

--- template_final_project-master/src/hook.py
+++ template_final_project-master/src/hook.py
@@ -2,14 +2,14 @@ import pygame
 import yarn
 
 class Hook(pygame.sprite.Sprite):
-    def __init__(self, image, x, y, var):
+    def __init__(self, image, x, y, yarn):
         super().__init__()
         #Need to figure out how to upload image
         self.image = pygame.image.load(f‘/assets/{name}.png’)
 		self.rect = self.image.get_rect()
 		self.rect.x = 0
 		self.rect.y = 0
-        self.var = yarn.motion()
+        self.yarn = yarn.motion()
 
 
     def move_with_yarn(self):
