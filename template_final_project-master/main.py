dafb754abe8ad4cbe240ac121cb18ef70e6e353e
Alison Batz <abatz@binghamton.edu>
2024-12-04 13:07:13 -0500

    Updated! Gotta work on my mainloop

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -3,8 +3,9 @@ from src.sample_controller import Controller
 
 def main():
     #Create an instance on your controller object
+    logic = Controller()
     #Call your mainloop
-    
+    logic.mainloop()
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
--- template_final_project-master/src/sample_controller.py
+++ template_final_project-master/src/sample_controller.py
@@ -13,43 +13,58 @@ class Controller:
     pygame.init()
     pygame.event.pump()
     self.screen = pygame.display.set_mode()
+    self.width, self.height = pygame.display.get_window_size()
     
     magic_ring = Button(color = "gold", text = "MR")
     chain_one = Button(color = "blue", text = "Chain 1")
     double_stitch = Button(color = "green", text = "Double")
     treble_stitch = Button(color = "pink", text = "Treble")
     
+    #Models
+    self.user = Hook()
+    self.ball_yarn = Yarn()
+    self.stitches = Stitch()
+    
     self.state = "START"
-    #Haven't finished this; have to include the models
     
   def mainloop(self):
+    pass
     #select state loop (switches between the states)
-    while True:
-      for event in pygame.events.get():
-        if event.type == pygame.Quit:
-          pygame.quit()
-          exit()
+    #while True:
+      #for event in pygame.events.get():
+        #if event.type == pygame.Quit:
+          #pygame.quit()
+          #exit()
         #elif event.type == pygame.get_pressed():
 					#pass
 
   ### below are some sample loop states ###
   
   def startloop(self): #where the player presses start; will use pygame menu
-      pass
       #event loop
-      #pygame.display.set_caption("Menu")
+      self.menu = pygame_menu.Menu("Yarning for More!", self.width-20, self.height/2)
+      self.menu.add.label("Click the button to start", max_char=-1, font_size=14)
+      self.menu.add.button('Start', self.start_game, align = pygame_menu.locals.ALIGN_CENTER)
       
-      #while True:
-        #pass
+      while self.state == "START":
+        for event in pygame.event.get():
+          if event.type == pygame.MOUSEBUTTONDOWN:
+            self.state = "GAME"
         
       #update data
-
+      self.menu.update(pygame.event.get())
       #redraw
+      self.menu.draw(self.screen)
+      pygame.display.flip()
     
     # 
   def gameloop(self):
-  
       #event loop
+      while self.state == "GAME":
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+                    exit()
+          elif event.type == pass
       #This is where the if statements for the button happen
       
       #update data
@@ -77,5 +92,4 @@ class Controller:
       #Stitch doesn't have to know about other stitches; the controller should worry
       
       #Motto: "Worry about yourself"
-  
-  pygame.quit
+  pygame.quit()
