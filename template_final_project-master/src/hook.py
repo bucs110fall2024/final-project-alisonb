f3d61104d5d677c8178e885cf981ffe2d673ad36
Alison Batz <abatz@binghamton.edu>
2024-12-05 22:59:35 -0500

    Found something that could make it transparent (I hope)

--- template_final_project-master/src/hook.py
+++ template_final_project-master/src/hook.py
@@ -3,7 +3,7 @@ import pygame
 class Hook(pygame.sprite.Sprite):
     def __init__(self, x = 800, y = 600, image = f"assets/hook-img.jpg"):
         super().__init__()
-        self.image = pygame.image.load(image)
+        self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
