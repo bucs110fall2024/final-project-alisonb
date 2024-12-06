8a42898f7b7ce1337dc0b0bc529a7dc062337dcd
Alison Batz <abatz@binghamton.edu>
2024-12-05 23:25:23 -0500

    Updated button class

--- template_final_project-master/src/button.py
+++ template_final_project-master/src/button.py
@@ -8,23 +8,3 @@ class Button(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = x, y #The position is given by rect
         self.color = color
         self.image.fill(self.color)
-        
-        text_color = (255-color[0], 255-color[1], 255-color[2]) #complimentary color
-        self.message = pygame.font.SysFont(None, 36).render(text, True, text_color)
-        
-        self.image.blit(self.message, (20, 20))
-
-    def highlight(self):
-        highlight_color = []
-        for i, c in enumerate(self.color):
-            if c+50 < 255:
-                highlight_color.append(c+50) 
-            else:
-                highlight_color.append(255)
-        self.image.fill(highlight_color)
-        self.image.blit(self.message, (20, 20))
-
-    def color_default(self):
-        self.image.fill(self.color)
-        self.image.blit(self.message, (20, 20))
-        
